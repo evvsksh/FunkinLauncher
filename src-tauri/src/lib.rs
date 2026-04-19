b4bffcfa8df06cdbb5384ec4ea14f19b2aaa6ea5
evvskx <evvskw@gmail.com>
2026-04-19 20:24:28 +0200

    added default.json capability

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,25 @@
+use std::fs;
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            let path_resolver = app.path();
+            let exe_dir = path_resolver
+                .executable_dir()
+                .expect("failed to get exe dir");
+            let mods_dir = exe_dir.join("mods");
+
+            if !mods_dir.exists() {
+                fs::create_dir_all(&mods_dir).expect("failed to create mods directory");
+            }
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
