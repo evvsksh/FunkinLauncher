1e1d87bc0c55152301f80c8056de97e09c994f75
evvskx <evvskw@gmail.com>
2026-04-19 19:50:20 +0200

    add tauri plugin shell dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,7 +8,8 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
