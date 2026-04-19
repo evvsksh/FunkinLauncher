05a28d149c9a925d6181cd7cfce8c9195a20bf5c
evvskx <evvskw@gmail.com>
2026-04-19 19:06:17 +0200

    create tauri conf + attempt fix for download

--- src/components/ModGrid.tsx
+++ src/components/ModGrid.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function ModGrid({ mods, onDownload }: Props) {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3.5">
+        <div className="grid grid-cols-3 gap-3.5">
             {mods.map((mod) => (
                 <ModCard key={mod._idRow} mod={mod} onDownload={onDownload} />
             ))}
