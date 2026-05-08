e2a55f4ba0636e9ee21609d75b5d0922a80229a2
evvskx <evvskw@gmail.com>
2026-05-08 11:51:53 +0200

    fix: general fixes 2

--- src/components/ModGrid.tsx
+++ src/components/ModGrid.tsx
@@ -3,10 +3,9 @@ import { ModCard } from "./ModCard";
 
 interface Props {
     mods: Mod[];
-    onDownload: (mod: Mod) => void;
 }
 
-export function ModGrid({ mods, onDownload }: Props) {
+export function ModGrid({ mods }: Props) {
     return (
         <div className="grid grid-cols-3 gap-3.5">
             {mods.map((mod) => (
