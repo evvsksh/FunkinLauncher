4656dd2456c96c42322c590b85bb907a2f364dde
evvskx <evvskw@gmail.com>
2026-05-08 10:13:44 +0200

    fix(vite): add more folders to ignore watchlist [skip ci]

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
 export default defineConfig(async () => ({
     plugins: [react(), tailwindcss()],
 
-    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-    //
-    // 1. prevent Vite from obscuring rust errors
     clearScreen: false,
-    // 2. tauri expects a fixed port, fail if that port is not available
     server: {
         allowedHosts: ["127.0.0.1"],
         port: 1420,
@@ -27,8 +22,15 @@ export default defineConfig(async () => ({
               }
             : undefined,
         watch: {
-            // 3. tell Vite to ignore watching `src-tauri`
-            ignored: ["**/src-tauri/**"],
+            ignored: [
+                "**/build-dir/**",
+                "**/.flatpak-builder/**",
+                "**/repo/**",
+                "**/var/run/**",
+                "**/proc/**",
+                "**/sys/**",
+                "**/src-tauri/**",
+            ],
         },
     },
 }));
